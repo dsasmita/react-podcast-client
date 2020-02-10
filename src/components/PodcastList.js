08e4444ccd8b7d584fee2d21e3f20410e556c365
dsasmita <dangs.work@gmail.com>
2020-02-10 21:47:20 +0700

    refactor to functional component

--- src/components/PodcastList.js
+++ src/components/PodcastList.js
@@ -2,23 +2,19 @@ import React from "react";
 
 import PodcastListContent from "./PodcastListContent.js";
 
-class PodcastList extends React.Component {
-  render() {
-    const { podcasts, filterText } = this.props;
+const PodcastList = props => {
+  const { podcasts, filterText } = props;
 
-    const filteredPodCast = podcasts.filter(function(podcast) {
-      return (
-        podcast.title.toLowerCase().indexOf(filterText.toLowerCase()) !== -1
-      );
-    });
-    return (
-      <ul className="podcast-list">
-        {filteredPodCast.map(podcast => (
-          <PodcastListContent key={podcast.id} podcast={podcast} />
-        ))}
-      </ul>
-    );
-  }
-}
+  const filteredPodCast = podcasts.filter(function(podcast) {
+    return podcast.title.toLowerCase().indexOf(filterText.toLowerCase()) !== -1;
+  });
+  return (
+    <ul className="podcast-list">
+      {filteredPodCast.map(podcast => (
+        <PodcastListContent key={podcast.id} podcast={podcast} />
+      ))}
+    </ul>
+  );
+};
 
 export default PodcastList;
--- src/components/PodcastListContent.js
+++ src/components/PodcastListContent.js
@@ -1,26 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-class PodcastListContent extends React.Component {
-  render() {
-    const { podcast } = this.props;
-    return (
-      <li>
-        <img className="thumbnail" src={podcast.thumbnail} alt="" />
-        <div className="info">
-          <h3>{podcast.title}</h3>
-          <a className="link" href={podcast.url} target="blank">
-            {podcast.url}
-          </a>
-          <div style={{ marginTop: "7px" }}>
-            <Link to={"/" + podcast.id} className="detail">
-              Lihat >>
-            </Link>
-          </div>
+const PodcastListContent = props => {
+  const { podcast } = props;
+  return (
+    <li>
+      <img className="thumbnail" src={podcast.thumbnail} alt="" />
+      <div className="info">
+        <h3>{podcast.title}</h3>
+        <a className="link" href={podcast.url} target="blank">
+          {podcast.url}
+        </a>
+        <div style={{ marginTop: "7px" }}>
+          <Link to={"/" + podcast.id} className="detail">
+            Lihat >>
+          </Link>
         </div>
-      </li>
-    );
-  }
-}
+      </div>
+    </li>
+  );
+};
 
 export default PodcastListContent;

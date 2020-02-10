c6cd7fbf9fd83fc8619a8d32f65efd2c13d2aad5
dsasmita <dangs.work@gmail.com>
2020-02-10 08:43:14 +0700

    Podcast detail page

--- src/pages/PodcastDetail.js
+++ src/pages/PodcastDetail.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 import Header from "../components/Header.js";
@@ -7,7 +8,7 @@ class PodcastDetail extends React.Component {
   constructor() {
     super();
     this.state = {
-      podcast: []
+      podcast: null
     };
   }
   async componentDidMount() {
@@ -15,16 +16,57 @@ class PodcastDetail extends React.Component {
       "https://json-server-heroku-zjqcjtotyv.now.sh/podcasts"
     );
     const id = this.props.match.params.id;
-    const podcast = data.data.filter(function(podcast1) {
-      return podcast1.id === parseInt(id);
+    const podcast = data.data.filter(function(podcastSelected) {
+      return podcastSelected.id === parseInt(id);
     });
-    console.log(podcast);
+    if (podcast.length === 1) {
+      this.setState({
+        podcast: podcast[0]
+      });
+    }
   }
 
   render() {
     return (
       <>
         <Header />
+        {this.state.podcast !== null && (
+          <Suspense fallback={<div>Loading...</div>}>
+            <div className="detail-podcast">
+              <div className="thumbnail">
+                <img
+                  src={this.state.podcast.thumbnail}
+                  alt={this.state.podcast.title}
+                />
+              </div>
+              <div className="info">
+                <h3>{this.state.podcast.title}</h3>
+                <a
+                  className="link"
+                  href={this.state.podcast.url}
+                  target="blank"
+                >
+                  {this.state.podcast.url}
+                </a>
+                <div style={{ marginTop: "7px" }}>
+                  <span>Episodes:</span>
+                  {this.state.podcast.episodes &&
+                    this.state.podcast.episodes.map(episode => (
+                      <audio controls key={episode.id}>
+                        <source src={episode.audio} type="audio/mp4" />
+                        Your browser does not support the audio element.
+                      </audio>
+                    ))}
+                </div>
+              </div>
+            </div>
+          </Suspense>
+        )}
+        <div style={{ marginTop: "7px" }}>
+          <Link to={"/"} className="detail">
+            {"<<"} Kembali
+          </Link>
+        </div>
       </>
     );
   }
